4bf12219043211345846e937eaab71f0b7e9d487
chetanrathod058 <rathodchetand9413@gmail.com>
2026-05-17 19:15:59 +0530

    update axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -46,7 +46,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
+console.log("ENV API URL =", import.meta.env.VITE_API_URL);
 api.interceptors.response.use(
   (response) => response,
   (error) => {
